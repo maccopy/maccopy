76ec4edac2677052cd08149a6b62ea1038e1737c
FernandoHaeser <fernandohaeserr@gmail.com>
2026-05-16 19:02:50 -0300

    chore: update cask sha256 for v1.1.1

--- Casks/clipboard-manager.rb
+++ Casks/clipboard-manager.rb
@@ -1,6 +1,6 @@
 cask "clipboard-manager" do
   version "1.1.1"
-  sha256 "PLACEHOLDER"
+  sha256 "154a21008f95485a50ce3da1ece1a21eb9a11d77ceb3a2b21472d7fe1bc13296"
 
   url "https://github.com/FernandoHaeser/macos-clipboard-manager/releases/download/v#{version}/ClipboardManager.zip"
   name "Clipboard Manager"
